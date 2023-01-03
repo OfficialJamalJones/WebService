4e27aa96654a79ac0a9dad74a091659f2699308d
OfficialJamalJones <jamalamanjones@gmail.com>
2023-01-03 12:52:47 -0500

    Initial Commit

---
+++ WebService/WebServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebServiceApp.swift
+//  WebService
+//
+//  Created by Consultant on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WebServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
